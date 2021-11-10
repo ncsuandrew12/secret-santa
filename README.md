72bb0638b03b4d096ca304d0dbdc7d66b821c40d
Andrew Felsher <ncsuandrew12@gmail.com>
2021-11-09 23:55:32 -0500

    Added README and consolidated python files into a single script.

---
+++ README.md
@@ -0,0 +1,136 @@
+# Introduction
+secret-santa.py is a lightweight script to organize a Secret Santa such that not even the organizer knows who is getting gifts for whom.
+
+# Setup & Run
+
+Running the script is pretty simple:
+1. Download the script
+2. Populate the input files.
+3. Run the script.
+
+## Download the script
+
+`git clone git@github.com:ncsuandrew12/secretsanta.git`
+
+or
+
+`wsget https://github.com/ncsuandrew12/secretsanta/blob/master/secret-santa.py`
+
+## Populate the input files
+
+The script assumes the presence of two JSON files used to configure Secret Santa: `cfg.json` and `participants.json`. These
+files must necessarily contain sensitive information, and so are not included in the repo.
+
+Example `cfg.json` file:
+
+```
+{
+  "description": "Emond's Field Secret Santa",
+  "ownerEmail": "mayor+secretsanta@emondsfield.gov",
+  "senderEmail": "bela+secret-santa@gmail.com",
+  "senderPassword": "",
+  "mailServerUrl": "smtp.gmail.com",
+  "mailServerPort": 465
+}
+```
+
+Example `participants.json` file:
+
+```
+{
+  "particpants": [
+    {
+      "name": "Rand al'Thor",
+      "email": "shepherd23@emondsfield.com"
+      "cc": [
+        "dragon@blacktower.org",
+        "dragon@tear.gov",
+        "dragon@cairhien.gov",
+        "king@illian.gov",
+        "caracarn@rhuidean.net",
+        "coramoor@athaanmiere.net" ]
+      "wishlists": [
+        {
+          "description": "list1",
+          "link": "https://www.amazon.com/hz/wishlist/ls/DR8MJQ8BXAA3L"
+        }, {
+          "description": "list2",
+          "link": "https://docs.google.com/document/d/18T_IgetKOsvNRYpjI1VnbhWssNJ4TP-lFPKSxQ/view"
+        },
+      ]
+    }, {
+      "name": "Tam al'Thor",
+      "email": "prodigal@emondsfield.com"
+      "cc": [ "tam.althor@illian.mil" ]
+    }, {
+      "name": "Bran al'Vere",
+      "email": "mayor@emondsfield.gov",
+      "cc": [ "bran@winespring.com" ]
+    }, {
+      "name": "Egwene al'Vere",
+      "email": "amyrlin@tarvalon.gov"
+      "cc": [ "men.are.woolheads@emondsfield.com" ]
+    }, {
+      "name": "Haral Luhhan",
+      "email": "blacksmith@emondsfield.com"
+    }, {
+      "name": "Perrin Aybara",
+      "email": "blacksmith2@emondsfield.com"
+    }, {
+      "name": "Alsbet Luhhan",
+      "email": "blacksmith.wrangler@emondsfield.com"
+    }, {
+      "name": "Matrim Cauthon",
+      "email": "gambler@band.mil"
+      "cc": [ "it.wasnt.me@emondsfield.com" ]
+    }, {
+      "name": "Abell Cauthon",
+      "email": "im.a.flaming.good.father.you.flaming.woolheads@boycottwotshow.com"
+      "cc": [ "archer2@emondsfield.com" ]
+    }, {
+      "name": "Padan Fain",
+      "email": "padan.fain@peddlers.org"
+      "bcc": [ "wormwood@mashadar.net", "padan.fain@shadow.org" ]
+    }, {
+      "name": "Cenn Buie",
+      "email": "thatcher@emondsfield.com"
+    }, {
+      "name": "El'Nynaeve ti al'Meara Mandragoran",
+      "email": "queen@malkier.gov"
+      "cc": [ "nyneave.almeara@tarvalon.gov", "wisdom@emondsfield.gov", "braidtugger@emondsfield.com" ]
+    }, {
+      "name": "Al'Lan Mandragoran",
+      "email": "king@malkier.gov"
+      "cc": [ "lan.mandragoran@tarvalon.gov", "lan.mandragoran@shienar.com" ]
+    }, {
+      "name": "Moiraine Merrilin",
+      "email": "moiraine.damodred@tarvalon.gov"
+      "cc": [ "bluestone@cairhien.com" ]
+    }, {
+      "name": "Thomdril Merrilin",
+      "email": "thom.merrilin@tarvalon.gov"
+      "cc": [ "thom.merrilin@gleemen.org", "court.bard@andor.gov" ]
+    }, {
+      "name": "Faile Aybara",
+      "email": "lady@tworivers.gov"
+      "cc": [ "faile.aybara@emondsfield.com", "zarene.bashere@saldaea.com", "mandarb@hornhunter.org" ]
+    }
+  ]
+}
+```
+
+## Run the script
+
+`python3 secret-santa.py`
+
+# Notes
+
+The script will create a `./log` directory in the current directory and any logs produced by the script will reside
+there. Some logs will also be printed to the console.
+
+`name` and `email` fields are required for all participants. Other fields are optional.
+
+All `cfg.json` fields are mandatory.
+
+The script will send one email to each participant. It will also send an email describing either success or an error to
+the owner as specified in the `ownerEmail` config field.
---
+++ secret-santa.py
@@ -0,0 +1,253 @@
+import json
+import logging
+import random
+import os
+import smtplib
+import ssl
+import sys
+import traceback
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+from logging.handlers import RotatingFileHandler
+
+context = ssl.create_default_context()
+description = None
+log = None
+mailServerPort = None
+mailServerUrl = None
+ownerEmail = None
+participants = None
+senderEmail = None
+senderPassword = None
+smtpSsl = None
+
+def main():
+  global log
+  global smtpSsl
+
+  random.seed()
+
+  log = SetupLogger("secret-santa")
+
+  try:
+    loadConfig()
+    setupSmtp()
+    loadParticipants()
+    pairParticipants()
+    sendNotifications()
+  except Exception as e:
+    log.error(e)
+    if not smtpSsl:
+      setupSmtp()
+    recipientEmail = ownerEmail
+    msg = '''To: {}
+Subject: {} script error!
+
+An error occurred while running the secret santa script:
+
+{}'''.format(recipientEmail, description, traceback.format_exc())
+    log.info("Sending email:\n%s", msg)
+    smtpSsl.sendmail(senderEmail, recipientEmail, msg, mail_options=['SMTPUTF8'])
+    raise e
+
+def loadConfig():
+  global description
+  global mailServerPort
+  global mailServerUrl
+  global ownerEmail
+  global senderEmail
+  global senderPassword
+
+  cfgFilename = "cfg.json"
+  log.info("Loading config: %s", cfgFilename)
+  with open(cfgFilename) as cfgFile:
+    cfg = json.load(cfgFile)
+  description = cfg["description"]
+  ownerEmail = cfg["ownerEmail"]
+  senderEmail = cfg["senderEmail"]
+  senderPassword = cfg["senderPassword"]
+  mailServerPort = cfg["mailServerPort"]
+  mailServerUrl = cfg["mailServerUrl"]
+
+def setupSmtp():
+  global smtpSsl
+
+  smtpSsl = smtplib.SMTP_SSL(mailServerUrl, mailServerPort, context=context)
+  smtpSsl.login(senderEmail, senderPassword)
+
+def loadParticipants():
+  global participants
+  participantsJsonFilename = "participants.json"
+  participants = []
+  log.info("Loading participants: {}".format(participantsJsonFilename))
+  with open(participantsJsonFilename) as participantsJsonFile:
+    participantsJson = json.load(participantsJsonFile)
+    for participantJson in participantsJson["participants"]:
+      participants.append(Participant(participantJson))
+  log.info("Loaded participants: %s", participants)
+
+def pairParticipants():
+  random.shuffle(participants)
+  log.info("Shuffled participants: %s", participants)
+  for participantIndex in range(len(participants)):
+    participants[participantIndex].recipient = participants[(participantIndex + 1) % len(participants)]
+  log.info("Paired participants: %s", participants)
+
+def sendNotifications():
+  random.shuffle(participants)
+
+  for participant in participants:
+    wishlistStrHtml = ""
+    wishlistStrText = ""
+    if participant.recipient.wishlists:
+      wishlistStrHtml = "<p>{}'s wishlist(s):</p><ol>".format(participant.recipient.name)
+      wishlistStrText = "\n\n{}'s wishlist(s):\n".format(participant.recipient.name)
+      wishlistNum = 1
+      for wishlist in participant.recipient.wishlists:
+        wishlistStrHtml += "<li><a href=\"{}\">{}</a></li>".format(wishlist.link, wishlist.description)
+        wishlistStrText += "{}. {}: {}\n".format(wishlistNum, wishlist.description, wishlist.link)
+        wishlistNum += 1
+      wishlistStrHtml += "</ol>"
+
+    msg = MIMEMultipart('alternative')
+    msg['To'] = participant.email
+    msg['From'] = senderEmail
+    if participant.cc:
+      msg['Cc'] = ",".join(participant.cc)
+    if participant.bcc:
+      msg['Bcc'] = ",".join(participant.bcc) # FIXME
+    msg['Subject'] = description
+
+    msgText = '''{},
+
+Your {} recipient is: {}.{}'''.format(
+      participant.name,
+      description,
+      participant.recipient.name,
+      wishlistStrText)
+
+    msgHtml = '''<html><head></head><body><p>{},<br />
+<br />
+Your {} recipient is: {}.</p>
+{}</body></html>'''.format(
+      participant.name,
+      description,
+      participant.recipient.name,
+      wishlistStrHtml)
+
+    msg.attach(MIMEText(msgText, 'plain'))
+    msg.attach(MIMEText(msgHtml, 'html'))
+    log.info("Sending email to %s (%s)", participant.name, participant.email)
+    smtpSsl.sendmail(senderEmail, participant.email, msg.as_string(), mail_options=['SMTPUTF8'])
+
+  participants.sort()
+  recipientList = ""
+  for participant in participants:
+    recipientList += "{} ({})\n".format(participant.name, participant.email)
+
+  recipientEmail = ownerEmail
+  msg = '''To: {}
+Subject: {} ran successfully!
+
+The secret santa script ran successfully and notified the following participants:
+
+{}'''.format(recipientEmail, description, recipientList)
+  log.info("Sending email:\n%s", msg)
+  smtpSsl.sendmail(senderEmail, recipientEmail, msg, mail_options=['SMTPUTF8'])
+
+class Participant:
+  def __init__(self, rjson):
+    self.name = rjson["name"]
+    self.email = rjson["email"]
+    self.cc = None
+    self.bcc = None
+    self.wishlists = None
+    self.recipient = None
+
+    if "cc" in rjson:
+      self.cc = rjson["cc"]
+    if "bcc" in rjson:
+      self.bcc = rjson["bcc"]
+    if "wishlists" in rjson:
+      self.wishlists = []
+      for wishlistJson in rjson["wishlists"]:
+        self.wishlists.append(Wishlist(wishlistJson))
+
+  def __repr__(self):
+    rjson = {}
+    rjson["name"] = self.name
+    rjson["email"] = self.email
+    if self.cc:
+      rjson["cc"] = self.cc
+    if self.bcc:
+      rjson["bcc"] = self.bcc
+    if self.wishlists:
+      rjson["wishlists"] = str(self.wishlists)
+    if self.recipient:
+      rjson["recipient"] = self.recipient.name
+    return json.dumps(rjson)
+
+  def __lt__(self, other):
+    return self.name < other.name
+
+class Wishlist:
+  def __init__(self, json):
+    self.description = json["description"]
+    self.link = json["link"]
+
+  def __repr__(self):
+    rjson = {}
+    rjson["name"] = self.description
+    rjson["link"] = self.link
+    return json.dumps(rjson)
+
+class MaxLogLevelFilter(logging.Filter):
+  def __init__(self, logLevel):
+    self.logLevel = logLevel
+  def filter(self, record):
+    return record.levelno <= self.logLevel
+  logLevel = logging.DEBUG
+
+class Formatter(logging.Formatter):
+  def format(self, record):
+    record.timeZone = "EST"
+    record.levelnameSuffix = (" " * (len("CRITICAL") - len(record.levelname)))
+    return logging.Formatter.format(self, record)
+
+def SetupLogger(title):
+  logDir = "./log/{}".format(title)
+  if not os.path.exists(logDir):
+    print("Making logDir: " + logDir)
+    os.makedirs(logDir)
+
+  logger = logging.getLogger(title)
+  # logger.setLevel(logging.DEBUG)
+  formatter = Formatter(
+    fmt="%(asctime)s %(timeZone)s %(processName)s:%(threadName)s %(levelname)s:%(levelnameSuffix)s %(pathname)s:%(lineno)d(%(funcName)s) %(message)s",
+    datefmt=None)
+  fileHandler = RotatingFileHandler(
+    filename="{}/log.log".format(logDir),
+    maxBytes=5 * 1024 * 1024, # 5MB
+    backupCount=9,
+    delay=True)
+
+  fileHandler.setFormatter(formatter)
+
+  stdoutHandler = logging.StreamHandler(stream=sys.stdout)
+  stdoutHandler.setFormatter(formatter)
+  stderrHandler = logging.StreamHandler(stream=sys.stderr)
+  stderrHandler.setFormatter(formatter)
+
+  logger.addHandler(fileHandler)
+  logger.addHandler(stdoutHandler)
+  logger.addHandler(stderrHandler)
+
+  logging.getLogger().setLevel(logging.NOTSET)
+
+  stdoutHandler.setLevel(logging.INFO)
+  stdoutHandler.addFilter(MaxLogLevelFilter(logging.WARNING))
+  stderrHandler.setLevel(logging.ERROR)
+
+  return logger
+
+main()
